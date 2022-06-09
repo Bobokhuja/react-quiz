f3a8ab7f227cb7e22f2a004d9ad38d38261e54cc
Bobokhuja <bobokhuja@yandex.ru>
2022-06-09 20:09:10 +0500

    quiz creator

--- src/containers/QuizCreator/QuizCreator.jsx
+++ src/containers/QuizCreator/QuizCreator.jsx
@@ -1,8 +1,51 @@
 import classes from './QuizCreator.module.css'
+import Button from '../../components/UI/Button/Button'
+import Input from '../../components/UI/Input/Input'
 
 function QuizCreator(props) {
+
+  const submitHandler = event => {
+    event.preventDefault()
+  }
+
+  const addQuestionHandler = () => {
+
+  }
+
+  const createQuizHandler = () => {
+
+  }
+
   return (
-    <div>Создать тест</div>
+    <div className={classes.QuizCreator}>
+      <div>
+        <h1>Создать тест</h1>
+
+        <form onSubmit={submitHandler}>
+
+          <input type='text' />
+          <hr />
+          <input type='text' />
+          <input type='text' />
+          <input type='text' />
+          <input type='text' />
+
+          <select></select>
+          <Button
+            type='primary'
+            onClick={addQuestionHandler}
+          >
+            Добавить вопрос
+          </Button>
+          <Button
+            type='success'
+            onClick={createQuizHandler}
+          >
+            Создать тест
+          </Button>
+        </form>
+      </div>
+    </div>
   )
 }
 
